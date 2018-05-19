8cecaef080bf5e03297bbc3168fc02630c80b041
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-19 01:09:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(x)
-  count = 0
-  while count < x
+  count = x
+  until count = 0
     puts "#{count} SECOND(S)!"
-    count += 1
+    count -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
