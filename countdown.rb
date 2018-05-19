ed8223515dce4011b262c35428537126e9b7dac5
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-19 01:13:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,12 @@ def countdown(x)
 end
 
 countdown(10)
+
+def countdown_with_sleep
+  count = x
+  until count == 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1.seconds)
+  end
+return "HAPPY NEW YEAR!"
